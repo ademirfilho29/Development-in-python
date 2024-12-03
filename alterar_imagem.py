dcf96d0f922a62d34eb4f28952d23e8107bf3977
ademirfilho29 <ademirfilho38334@gmail.com>
2024-12-03 06:40:21 -0300

    alterações no codigo

--- alterar_imagem.py
+++ alterar_imagem.py
@@ -3,12 +3,17 @@ from PIL import Image
 
 if __name__ == "__main__":
 
-    pasta_fotos_para_alterar= "C:\\Users\\USER\\Desktop\\fotos editadas"
+    pasta_fotos_para_alterar= "C:\\Users\\USER\\Desktop\\mais fotos felipe"
+        
+    # caminho_pasta_salvar= "C:\\Users\\USER\\Desktop\\fotos editadas"
+
     #alterar a resolução das imagens 
     nova_resu = (188,188)
 
+    #iterar sobre cada foto da pasta 
     for foto in os.listdir(pasta_fotos_para_alterar):
 
+        #procurar se todas são no formato png, jpg ou jpeg
         if foto.lower().endswith(('png', 'jpg', 'jpeg')):
 
             caminho_da_imagem =  os.path.join(pasta_fotos_para_alterar, foto)
@@ -24,7 +29,6 @@ if __name__ == "__main__":
                 img_redimensionada = img.resize(nova_resu)
 
                 salvar_nome = f"imagem_editada {[]}"
-                # caminho_novo = os.path.join("C:\\Users\\USER\\Desktop\\fotos editadas")
                 img_redimensionada.save(caminho_da_imagem)
 
                 print(f"Imagem {foto} redimensionada e salva como {salvar_nome}")
